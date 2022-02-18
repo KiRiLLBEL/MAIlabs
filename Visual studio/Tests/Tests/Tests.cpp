d1f9cb4f57393e6da658b9e937ad1aa89544e960
KiRiLLBEL <kirillbelonosov@yandex.ru>
2022-02-19 00:49:59 +0300

    Complete 20 lab

--- Visual studio/Tests/Tests/Tests.cpp
+++ Visual studio/Tests/Tests/Tests.cpp
@@ -1,6 +1,18 @@
 ﻿#include <iostream>
 using namespace std;
 
+void solve() {
+	int A, B;
+	cin >> A >> B;
+	cout << A + B << "\n";
+}
+
 int main() {
-	cout << "Hello world!" << "\n";
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int T;
+	cin >> T;
+	for (int i = 0; i < T; ++i) {
+		solve();
+	}
 }
