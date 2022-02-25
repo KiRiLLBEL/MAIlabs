41ae5c17bae354100ecb8cbbdd71941fcf62e8f3
KiRiLLBEL <kirillbelonosov@yandex.ru>
2022-02-25 23:29:24 +0300

    Olimp prog

---
+++ OlimpProg/Atcoder/240A/240A/240A.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	if (abs(a - b) == 1) {
+		cout << "Yes" << "\n";
+	}
+	else if (abs(a - b) == 9) {
+		cout << "Yes" << "\n";
+	}
+	else {
+		cout << "No" << "\n";
+	}
+	return 0;
+}
---
+++ OlimpProg/Codeforces/50A/50A.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n, m;
+	cin >> n >> m;
+	cout << (n * m) / 2 << "\n";
+}
