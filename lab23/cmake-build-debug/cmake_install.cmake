cca3cf0f0dc63a25558901bae4df1ee699978d97
KiRiLLBEL <kirillbelonosov@yandex.ru>
2022-03-13 10:05:56 +0300

    lab 23 lib

--- lab23/cmake-build-debug/cmake_install.cmake
+++ lab23/cmake-build-debug/cmake_install.cmake
@@ -37,6 +37,13 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2021.3.3/bin/mingw/bin/objdump.exe")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("C:/Users/kiril/Projects/lab23/cmake-build-debug/build/cmake_install.cmake")
+  include("C:/Users/kiril/Projects/lab23/cmake-build-debug/nodeLibrary/cmake_install.cmake")
+
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
