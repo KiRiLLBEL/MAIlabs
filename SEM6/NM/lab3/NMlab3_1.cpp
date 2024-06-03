2d524c70fd51b56f0e662b046bcd02fd02f6fbae
Kirill Belonosov <kirillbelonosov@yandex.ru>
2024-06-03 18:22:39 +0300

    Added NM reports

--- SEM6/NM/lab3/NMlab3_1.cpp
+++ SEM6/NM/lab3/NMlab3_1.cpp
@@ -16,8 +16,6 @@ int main() {
     vector<double> Y1 = {0, 0.4142, 1, 2.4142};
     vector<double> X2 = {0, M_PI/8, M_PI/3, 3*M_PI/8};
     vector<double> Y2 = {0, 0.4142, 1.7320, 2.4142};
-//    vector<double> X2 = {0.1, 0.5, 0.9, 1.3};
-//    vector<double> Y2 = {100.01, 4.25, 2.0446, 2.2817};
     double x = 3 * M_PI / 16;
     cout << "Lagrange first: ";
     cout << lagrangePolynomial(X1, Y1, x);
